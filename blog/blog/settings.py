a95abce151022d2dbca79a024b03a369986f1299
zzy666ggg <2054872647@qq.com>
2024-05-20 21:21:22 +0800

    '静态资源配置‘

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -123,6 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+#设置静态资源路径
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+
 
 #Redis位置
 CACHES = {
