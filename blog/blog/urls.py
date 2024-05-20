0a3af9f6e71129a6199a14b271ca9ccbd1c907e2
zzy666ggg <2054872647@qq.com>
2024-05-20 23:25:33 +0800

    '注册界面展示‘

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -14,22 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 #1.导入系统logging
 import logging
 #2.创建日志记录器
-logger = logging.getLogger('django')
-
-
-
-from django.http import HttpResponse
-def log(requse):
-    #3.使用日志器记录信息
-    logger.info('info')
-    return HttpResponse('test')
+# logger = logging.getLogger('django')
+#
+#
+#
+# from django.http import HttpResponse
+# def log(requse):
+#     #3.使用日志器记录信息
+#     logger.info('info')
+#     return HttpResponse('test')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',log),
+    #include的参数中 我们首先来设置一个元素 urlconf_module, app_name
+    #urlconf_module 子应用的路由
+    #app_name 子应用的名字
+
+    #namespace 命名空间
+    path('', include(('users.urls', 'users'), namespace='users'))
+    #path('',log),
 ]
---
+++ blog/users/urls.py
@@ -0,0 +1,12 @@
+#用于进行user子应用的视图路由
+
+from django.urls import path
+from users.views import RegisterView
+
+
+urlpatterns = [
+    # 参数1：路由
+    # 参数2：视图函数
+    # 参数3：路由名，方便通过reverse来获取路由
+    path('register/', RegisterView.as_view(), name='register'),
+]
